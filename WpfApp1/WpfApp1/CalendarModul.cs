79e1b35674941b34cb4fd481ddad4d71760de0f8
harD1kkkk <137618635+harD1kkkk@users.noreply.github.com>
2024-04-07 12:43:56 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/CalendarModul.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace WpfApp1
+{
+
+    internal class CalendarModul
+    {
+        public string AddDateTolist(string date, List<string> selectedDates)
+        {
+            MainWindow mainWindow = new MainWindow();
+            string selectedDate = date;
+
+            if (selectedDates.Contains(selectedDate))
+            {
+                return "0";
+            }
+            return selectedDate;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> selectedDates = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            CalendarModul calendarModul = new CalendarModul();
+
+            string date = calendarModul.AddDateTolist(ChooseCalendar.SelectedDate.Value.ToString(), selectedDates);
+
+            if (date != "0")
+            {
+                ListBox.Items.Add(date);
+                selectedDates.Add(date);
+            }
+            else
+            {
+                MessageBox.Show("Already reserved");
+            }
+        }
+    }
+}
